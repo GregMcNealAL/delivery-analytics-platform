ae902337fc288552ebe55486dca4861ee96909cc
Greg McNeal <225691662+GregMcNealAL@users.noreply.github.com>
2025-11-16 22:16:22 -0600

    refactor: moved get_db function from main to db.py file

--- orders_service/db.py
+++ orders_service/db.py
@@ -1,10 +1,23 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./orders.db"
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./orders.db")
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+connect_args = {"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else{}
+
+engine = create_engine(
+    DATABASE_URL,
+    connect_args=connect_args
+)
 
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 
 Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
