d4040189035b6bfce663bc7e73e170fffc538694
Greg McNeal <225691662+GregMcNealAL@users.noreply.github.com>
2025-11-18 09:14:47 -0600

    refactor: moved get_db function to db.py

--- orders_service/main.py
+++ orders_service/main.py
@@ -1,24 +1,15 @@
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
-from orders_service.db import SessionLocal, engine
-from orders_service.models import Order, Base
+from orders_service.db import get_db
+from orders_service.models import Order
 from orders_service.schemas import OrderRead, OrderCreate
 from typing import List
 
-Base.metadata.create_all(bind=engine)
 app = FastAPI(title="Order Service")
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
 
 @app.get("/orders", response_model=List[OrderRead])
 def get_orders(db: Session = Depends(get_db)):
-    orders = db.query(Order).all()
-    return orders
+    return db.query(Order).all()
 
 @app.get("/orders/{order_id}", response_model=OrderRead)
 def get_order(order_id: int, db: Session = Depends(get_db)):
@@ -32,5 +23,5 @@ def create_order(order: OrderCreate, db: Session = Depends(get_db)):
     new_order = Order(**order.dict())
     db.add(new_order)
     db.commit()
-    db.refresh(new_order)  # refresh so new_order has the auto-generated ID
+    db.refresh(new_order)
     return new_order
