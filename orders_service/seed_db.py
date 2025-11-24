14740a727f9201b95837bbf3d8ae90e777c14e48
Greg McNeal <225691662+GregMcNealAL@users.noreply.github.com>
2025-11-23 19:26:33 -0600

    refactor(seed_db): seed db with more realistic item names and locations

--- orders_service/seed_db.py
+++ orders_service/seed_db.py
@@ -1,28 +1,33 @@
-from faker import Faker
 import random
+from faker import Faker
 from orders_service.db import SessionLocal, engine, Base
 from orders_service.models import Order
 
-fake = Faker()
+fake = Faker("en_US")
 
-def create_fake_orders(n=200):
+STATUSES = ["delivered", "pending", "cancelled"]
+
+def generate_product_name():
+    words = [fake.word().title() for _ in range(random.randint(2, 3))]
+    return " ".join(words)
+
+def seed_orders(n=300):
     session = SessionLocal()
     try:
         for _ in range(n):
             order = Order(
-                item_name = fake.word().title() + " " + fake.word().title(),
-                location = fake.city(),
-                cost = round(random.uniform(5, 200), 2),
-                delivery_time = random.randint(10, 120),  # minutes
-                status = random.choice(["delivered", "pending", "cancelled"])
+                item_name=generate_product_name(),
+                location=fake.city(),
+                cost=round(random.uniform(10, 500), 2),
+                delivery_time=random.randint(15, 120),  # minutes
+                status=random.choice(STATUSES)
             )
             session.add(order)
         session.commit()
+        print(f"DB seeded successfully")
     finally:
         session.close()
 
 if __name__ == "__main__":
-    # Ensure tables exist (safe to call even if created already)
     Base.metadata.create_all(bind=engine)
-    create_fake_orders(300)
-    print("✅ Seeded DB with fake orders")
+    seed_orders()
