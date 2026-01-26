aeede25140e1c86ee81bc6e9a66c4d6d758e8e5f
Greg McNeal <225691662+GregMcNealAL@users.noreply.github.com>
2026-01-26 01:46:56 -0600

    fix settings inherit from BaseSettings

--- orders_service/core/config.py
+++ orders_service/core/config.py
@@ -1,6 +1,6 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-class Settings(SettingsConfigDict):
+class Settings(BaseSettings):
     ORDERS_API_KEY: str
 
     model_config = SettingsConfigDict(
